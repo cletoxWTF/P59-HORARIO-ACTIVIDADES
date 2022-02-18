5b6161b0ab836cddb9f0c6577fda3f20f6c3ad5c
cletoxWTF <jolallaq@est.ups.edu.ec>
2022-02-17 20:00:40 -0500

    DISEÑO VENTANA AGREGAR

--- examen_olalla_john.pro
+++ examen_olalla_john.pro
@@ -9,13 +9,16 @@ CONFIG += c++11
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    agregar.cpp \
     main.cpp \
     principal.cpp
 
 HEADERS += \
+    agregar.h \
     principal.h
 
 FORMS += \
+    agregar.ui \
     principal.ui
 
 # Default rules for deployment.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "principal.h"
 
 #include <QApplication>
+/*https://www.flaticon.es/iconos-gratis/eliminar iconos creados por Freepik
+ * https://www.flaticon.es/iconos-gratis/salida  iconos creados por Freepik
+ * https://www.flaticon.es/iconos-gratis/anadir  iconos creados por Smashicons
+ * https://www.flaticon.es/iconos-gratis/final iconos creados por Freepik
+ * https://www.flaticon.es/iconos-gratis/lista  iconos creados por Freepik
+ * https://www.flaticon.es/iconos-gratis/anadir  iconos creados por Freepik
+ * https://www.flaticon.es/iconos-gratis/visto  iconos creados por Freepik
+ */
 
 int main(int argc, char *argv[])
 {
